90421b806e4499475bd04495720b1459c79b1cce
akep0280 <akepley@gmail.com>
2015-06-18 10:10:39 -0500

    syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,21 @@
 class ntp {
-  include ntp
+  package { 'ntpd':
+    ensure => 'present',
+    }
+
+  file { '/etc/ntp.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    source  => 'puppet:///modules/ntp/ntp_conf',
+    require => Package['ntpd'],
+    notify  => Service['ntpd'],
+    }
+
+  service { 'ntpd':
+    ensure  => 'running',
+    enable  => true,
+    require => File['/etc/ntp.conf'],
+    }
 }
